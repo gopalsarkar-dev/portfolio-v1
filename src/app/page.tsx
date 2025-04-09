207fedfd3a5f5cacfb2f9b30e832486ef19064de
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-04-09 12:25:05 +0530

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,32 +16,32 @@ const page = () => {
     <>
       <div className="grid h-screen place-items-center">
         <div className="grid place-items-center gap-8">
-          <div className="text-center text-5xl font-bold text-neutral-900 dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 dark:bg-clip-text dark:text-transparent sm:text-6xl">
-            Hi<span className="text-2xl">,</span> I am Gopal Sarkar
-            <span className="text-2xl">,</span> Frontend
+          <div className="text-center text-4xl font-bold text-neutral-900 dark:bg-gradient-to-b dark:from-neutral-50 dark:to-neutral-400 dark:bg-clip-text dark:text-transparent sm:text-6xl">
+            Hi<span className="text-xl">,</span> I am Gopal Sarkar
+            <span className="text-xl">,</span> Frontend
             <br className="" /> & Backend Developer.
           </div>
 
           <Image src="/favicon.ico" alt="logo" width={200} height={200} />
 
-          <div className="grid grid-flow-col gap-3 sm:gap-5">
+          <div className="flex items-center justify-center gap-5">
             <Link href="tel: 7477757792">
-              <Button size={"lg"} variant={"outline"}>
+              <Button size={"icon"} variant={"outline"}>
                 <BiSolidPhoneCall />
               </Button>
             </Link>
             <Link href="mailto:gopalsarkar.dev@gmail.com">
-              <Button size={"lg"} variant={"outline"}>
+              <Button size={"icon"} variant={"outline"}>
                 <BiLogoGmail />
               </Button>
             </Link>
             <Link href="https://github.com/gopalsarkar-dev">
-              <Button size={"lg"} variant={"outline"}>
+              <Button size={"icon"} variant={"outline"}>
                 <FaGithub />
               </Button>
             </Link>
             <Link href="https://linkedin.com/in/gopalsarkar-dev">
-              <Button size={"lg"} variant={"outline"}>
+              <Button size={"icon"} variant={"outline"}>
                 <FaLinkedin />
               </Button>
             </Link>
